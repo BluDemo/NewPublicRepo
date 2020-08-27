e4cd09459c6ea21782d90ff9ed4e2a6a6682842f
mauricecevans <59456239+mauricecevans@users.noreply.github.com>
2020-08-27 10:48:46 -0600

    Update query_builder.py

--- query_builder.py
+++ query_builder.py
@@ -3,7 +3,6 @@ from logging import getLogger, NullHandler
 logger = getLogger(__name__)
 logger.addHandler(NullHandler())
 
-awS_secret="7N1675JKTRM7PP8FF9E8M9C3F1EQ8PMP90P40P0K"
 
 def render_query(dataset, tables, select=None, conditions=None,
                  groupings=None, having=None, order_by=None, limit=None):
